501e5192ee198767b223a1154916f0c2bd969c8b
AkramZennad <akram.zennad@univ-constantine2.dz>
2025-05-04 21:21:30 +0100

    revised the update of database location

--- backend/part1/address.php
+++ backend/part1/address.php
@@ -4,7 +4,8 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-require_once 'Database.php';
+require_once(__DIR__ . '/../Database.php');
+
 $db = new Database();
 $conn = $db->connect();
 
